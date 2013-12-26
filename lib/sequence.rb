f74a1041893b079cbdcb354128f6352e2b86986b
dimas <cartobus@gmail.com>
2013-12-26 16:41:34 +0200

    slightly shortened module code and complemented test suite

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -10,80 +10,58 @@ module Sequence
     def sequence(item, array_of_items)
 
       define_method("initialize") do
-        @current_index = 0
+        @current_item_index = 0
         @array_of_items = array_of_items
-        validate_array(array_of_items)
-        self.singleton_class.send(:attr_reader, :array_of_items, :current_index)
+        validate_array(array_of_items) rescue return
+        self.singleton_class.send(:attr_reader, :array_of_items, :current_item_index)
       end
 
       define_method(item) do
-        array_of_items[current_index]
+        array_of_items[current_item_index]
       end
 
-      define_method("#{item}=") do |item|
-        item_index = validate_item(item)
-        return false unless item_index
-        @current_index = item_index
-        array_of_items[current_index]
+      define_method("#{item}=") do |itm|
+        get_index(itm) rescue return false
+        self.send(item)
       end
 
       define_method("next_#{item}") do
-        res = validate_iteration
-        return res if res
-        @current_index += 1
+        validate_iteration rescue return false
+        @current_item_index += 1
         self.send(item)
       end
 
       define_method('validate_iteration') do
-        if current_index >= array_of_items.size-1
-          'iteration reached an end'
+        if current_item_index >= array_of_items.size-1
+          raise 'iteration reached an end'
         end
       end
 
       define_method('validate_array') do |array|
-        begin
-          array = check_array_content(array)
-        rescue ArgumentError => e
-          puts e
-          return false
-        end
-      end
-
-      define_method('check_array_content') do |array|
         array.each do |array_item|
           unless array_item.is_a? Symbol
             raise ArgumentError, 'array should only consists of symbols'
           end
         end
-        array
       end
 
-      define_method('validate_item') do |item|
-        begin
-          item       = check_type(item)
-          item_index = check_index(item)
-        rescue ArgumentError => e
-          puts e
-        rescue Exception
-          return
-        end
+      define_method('get_index') do |item|
+        check_type(item)
+        check_index(item)
       end
 
       define_method('check_type') do |item|
         unless item.is_a? Symbol
-          raise ArgumentError, 'assigned value should be a symbol'
+          raise 'assigned value should be a symbol'
         end
-        item
       end
 
       define_method('check_index') do |item|
         item_index = array_of_items.index(item)
-        if !item_index
-          raise Exception, 'sequence not contain such item'
-        elsif item_index >= current_index+2
-          raise Exception
+        if !item_index || item_index >= current_item_index+2
+          raise 'wrong assignment'
         end
-        item_index
+        @current_item_index = item_index
       end
     end
   end
--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -15,19 +15,61 @@ describe 'Sequence' do
   it 'responds to dynamically created methods' do
     expect(car).to respond_to(:gear)
     expect(car).to respond_to(:next_gear)
+    expect(car).to respond_to(:current_item_index)
+    expect(car).to respond_to(:array_of_items)
   end
 
-  it 'iterates through sequence' do
-    expect(car.gear).to eq :first
-    expect(car.next_gear).to eq :second
-    expect(car.gear).to eq :second
-    expect(car.send(:gear=, :fourth)).to eq false
-    expect(car.gear).to eq :second
+  context 'get values from sequence' do
+    before(:all) do
+      @car = Car.new
+    end
 
-    car.gear = :third
-    expect(car.gear).to eq :third
+    it "returns a first value of sequence" do
+      expect(@car.gear).to eq :first
+    end
 
-    expect(car.next_gear).to eq :fourth
-    expect(car.next_gear).to eq 'iteration reached an end'
+    it "return next value in sequence" do
+      expect(@car.next_gear).to eq :second
+    end
+
+    it "assigns value if it's next to current and returns it" do
+      @car.gear = :third
+      expect(@car.gear).to eq :third
+    end
+  end
+
+  context 'exception handling' do
+    it 'returns false if one tries to assign value that higher than current but not next to it' do
+      car.gear = :first
+      expect( car.send(:gear=, :third) ).to eq false
+    end
+
+    it 'stops iteration when sequence reached an end' do
+      car.next_gear
+      car.next_gear
+      car.next_gear
+      expect(car.next_gear).to eq false
+    end
+  end
+
+  context 'validation errors' do
+    it 'validates an array' do
+      array = [:first, :second, 'third', 4]
+      expect do 
+        car.validate_array(array)
+      end.to raise_error('array should only consists of symbols')
+    end
+
+    it 'assings a values of proper type' do
+      expect do 
+        car.check_type('first')
+      end.to raise_error('assigned value should be a symbol')
+    end
+
+    it 'checks if item belongs to array' do
+      expect do 
+        car.check_index(:fifth)
+      end.to raise_error('wrong assignment')
+    end
   end
 end
